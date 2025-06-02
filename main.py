d90b295a40582de715809c67301342e8817e944e
Muelvzz <muelvinlopez25@gmail.com>
2025-06-02 19:59:33 +0800

    Add descriptions, images, and url's

--- main.py
+++ main.py
@@ -18,14 +18,21 @@ with col2:
 content2 = "If there are any questions, and concerns - feel free to let me know in my email or in my social media accounts."
 st.info(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([2, 0.3, 2])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
